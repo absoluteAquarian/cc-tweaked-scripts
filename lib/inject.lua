8f6f3640b54d34389ffb98a21afee8a6657d633b
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2026-03-15 03:49:58 -0500

    `inject` no longer triggers the "Too long without yielding" error message

--- lib/inject.lua
+++ lib/inject.lua
@@ -18,8 +18,6 @@
 --- ```
 local module_table = {}
 
-local __ignoring = false
-
 --- @class DebugStackEntry
 --- @field depth integer
 --- @field tail boolean
@@ -32,15 +30,68 @@ local __max_stacked = 0
 
 local __file
 
+local __iter = 0
+local __iter_count = 0
+local MAX_ITER = 6000
+
 module_table.MAX_DEPTH = 2000
 
+local function record_current_call_stack()
+    for _, entry in ipairs(__stack) do
+        local caller = entry.caller
+        local callee = entry.callee
+
+        local depth = entry.depth
+        local src = caller.short_src
+        local after = src:match(".*/()")
+        if after then src = src:sub(after) end
+        local line = caller.currentline
+        local name = callee.name or "?"
+
+        local report = line > 0
+            and string.format("%d: %s:L%d (%s)", depth, src, line, name)
+            or string.format("%d: %s (%s)", depth, src, name)
+
+        __file.write(report)
+
+        if entry.tail then __file.write(" [tail call]") end
+
+        __file.writeLine("")
+    end
+
+    __file.writeLine("")
+    __file.writeLine("")
+end
+
 local function __debug_hook(event)
-    if __ignoring then return end
-
-    __ignoring = true
-
     -- Indicate in the stack whether a call is a tail call, for future reference
     if event == "call" or event == "tail call" then
+        __iter = __iter + 1
+        if #__stack >= 20 or __iter >= MAX_ITER then
+            __iter = 1
+            __iter_count = __iter_count + 1
+
+            print(string.format(
+                "Debug yield %d times (#stack = %d)",
+                __iter_count,
+                #__stack
+            ))
+
+            local evt, data = os.pullEventRaw()
+            if evt == "terminate" then
+                error("Debug Terminated", 0)
+            elseif evt == "key" and data == keys.s then
+                __file.writeLine(string.format(
+                    "Call stack (iteration %d):",
+                    __iter_count
+                ))
+                __file.writeLine("")
+                __file.writeLine("")
+
+                record_current_call_stack()
+            end
+        end
+
         table.insert(
             __stack,
             {
@@ -69,11 +120,13 @@ local function __debug_hook(event)
             current = table.remove(__stack)--[[@as DebugStackEntry]]
         end
     end
-
-    __ignoring = false
 end
 
 --- Prepares the module for tracking functions
+--- <p/>
+--- While tracking is active, the Lua interpreter will be forced to yield periodically to allow the OS to tick events and other important logic.<br/>
+--- To continue execution, invoke any user-related event (such as clicking the terminal UI or pressing a key).<br/>
+--- To record the current call stack to the log file, press the 'S' key.
 function module_table.prepare()
     local FILE = "report.log"
     if fs.exists(FILE) then fs.delete(FILE) end
@@ -92,29 +145,9 @@ function module_table.cleanup()
         __file.writeLine("")
         __file.writeLine("")
 
-        for _, entry in ipairs(__stack) do
-            local caller = entry.caller
-            local callee = entry.callee
-
-            local depth = entry.depth
-            local src = caller.short_src
-            local after = src:match(".*/()")
-            if after then src = src:sub(after) end
-            local line = caller.currentline
-            local name = callee.name or "?"
-
-            local report = line > 0
-                and string.format("%d: %s:L%d (%s)", depth, src, line, name)
-                or string.format("%d: %s (%s)", depth, src, name)
-
-            __file.write(report)
-
-            if entry.tail then __file.write(" [tail call]") end
-
-            __file.writeLine("")
-        end
+        record_current_call_stack()
     end
-    
+
     __file.close()
 end
 
