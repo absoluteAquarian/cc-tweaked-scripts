c9e2facefffc4181b582e538e1028cb8d780074d
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2026-03-21 17:55:20 -0500

    added more features to `comms` and prettified its display logic

--- programs/comms.lua
+++ programs/comms.lua
@@ -3,6 +3,150 @@ local R_terminal = require "lib.cc.terminal"
 local exec = require "lib.exec"
 local R_table = require "lib.table"
 
+local native = {
+    math = {
+        max = math.max,
+        min = math.min,
+        floor = math.floor
+    },
+    table = {
+        insert = table.insert,
+        remove = table.remove,
+        concat = table.concat,
+    }
+}
+
+local w, h = term.getSize()
+
+--- @type string[]
+local __messages = {}
+--- @type { fg: number, bg: number }[]
+local __msgcol = {}
+local __colfg = colors.white
+local __colbg = colors.black
+
+local __msgview = 1
+local __msgviwemax = h - 2
+
+--- @param msg string
+local function record(msg)
+    local first, last = 1, native.math.min(w, #msg)
+    while first <= #msg do
+        -- Restrict the message history
+        if #__messages >= 200 then
+            table.remove(__messages, 1)
+            table.remove(__msgcol, 1)
+            if __msgview > 1 then __msgview = __msgview - 1 end
+        end
+
+        table.insert(__messages, msg:sub(first, last))
+        table.insert(__msgcol, { fg = __colfg, bg = __colbg })
+        first, last = last + 1, native.math.min(last + w, #msg)
+    end
+end
+
+--- @param fmt string
+--- @param ... any
+local function recordfmt(fmt, ...)
+    record(string.format(fmt, ...))
+end
+
+--- @param fg number
+--- @param bg number
+local function set_record_colors(fg, bg)
+    __colfg = fg
+    __colbg = bg
+end
+
+--- @param msg string
+local function recorderr(msg)
+    set_record_colors(colors.red, colors.black)
+    record(msg)
+    set_record_colors(colors.white, colors.black)
+end
+
+--- @param fmt string
+--- @param ... any
+local function recorderrfmt(fmt, ...)
+    recorderr(string.format(fmt, ...))
+end
+
+--- @type string[]
+local __input = {}
+local __inputview = 1
+local INPUTPREFIX = "> "
+local __inputcursor = #INPUTPREFIX + 1
+
+--- @return integer view_start
+--- @return integer view_end
+--- @return integer cursor_in_view
+local function __get_input_positions()
+    local view_start = __inputview
+    local view_end = native.math.min(view_start + w - #INPUTPREFIX - 1, #__input + 1)
+    local cursor_in_view = view_start + __inputcursor - #INPUTPREFIX - 1
+
+    return view_start, view_end, cursor_in_view
+end
+
+local function refresh_message_display()
+    -- Erase the lines
+    for i = 1, __msgviwemax do
+        term.setCursorPos(1, i)
+        term.clearLine()
+    end
+
+    -- Show the messages
+    local y = 1
+    local fg, bg = colors.white, colors.black
+
+    for i = __msgview, native.math.min(__msgview + __msgviwemax - 1, #__messages) do
+        local cols = __msgcol[i]
+
+        term.setCursorPos(1, y)
+
+        if fg ~= colors.white then term.setTextColor(colors.white) end
+        if bg ~= colors.black then term.setBackgroundColor(colors.black) end
+
+        fg, bg = cols.fg, cols.bg
+
+        term.clearLine()
+
+        if fg ~= colors.white then term.setTextColor(fg) end
+        if bg ~= colors.black then term.setBackgroundColor(bg) end
+
+        write(__messages[i])
+        y = y + 1
+    end
+
+    while y <= h do
+        term.setCursorPos(1, y)
+        term.clearLine()
+        y = y + 1
+    end
+end
+
+local function refresh_input_display()
+    -- Erase the line
+    term.setCursorPos(1, h)
+    term.clearLine()
+
+    -- Show the input
+    term.setCursorPos(1, h)
+    write(INPUTPREFIX)
+
+    local view_start, view_end, _ = __get_input_positions()
+
+    for i = view_start, view_end do
+        if i <= #__input then
+            write(__input[i])
+        else
+            write(" ")  -- Allow cursor to be "after" the input text, even when it would normally span the entire display
+        end
+    end
+
+    term.setCursorPos(__inputcursor, h)
+end
+
 --- @type integer
 local __id = os.getComputerID()
 
@@ -130,6 +274,244 @@ local function receive(sender, command, ...)
     end
 end
 
+--- @param new_cursor integer
+--- @return boolean
+local function __set_cursor(new_cursor)
+    local old = __inputcursor
+
+    __inputcursor = native.math.max(1 + #INPUTPREFIX, native.math.min(new_cursor, w))
+
+    return old ~= __inputcursor
+end
+
+local MAX_INPUT_LENGTH = w - #INPUTPREFIX
+
+--- @param new_view integer
+--- @return boolean
+local function __set_view(new_view)
+    local old = __inputview
+
+    local view_max = native.math.max(1, #__input - MAX_INPUT_LENGTH + 1)
+    __inputview = native.math.max(1, native.math.min(new_view, view_max))
+
+    return old ~= __inputview
+end
+
+local KEYHOLDMAX = 15
+local __keyhold = KEYHOLDMAX
+local __keyholdmax = KEYHOLDMAX
+
+--- @param direction 1|-1
+--- @param held boolean
+--- @param recurse boolean
+local function __key_cursormove(direction, held, recurse)
+    if not held then
+        if not recurse then
+            -- Reset key hold state
+            __keyhold = KEYHOLDMAX
+            __keyholdmax = KEYHOLDMAX
+        end
+
+        -- Immediately move the cursor
+        if not __set_cursor(__inputcursor + direction) then
+            -- Cursor is at a hard limit, so move the view if possible
+            __set_view(__inputview + direction)
+        end
+    else
+        -- Held keys should move the cursor after a short delay, and then repeatedly after that delay until released
+        __keyhold = __keyhold - 1
+
+        if __keyhold <= 0 then
+            __key_cursormove(direction, false, true)
+
+            __keyhold = __keyholdmax
+
+            if __keyholdmax > 2 then __keyholdmax = native.math.max(2, native.math.floor(__keyholdmax / 2)) end
+        end
+    end
+
+    refresh_input_display()
+end
+
+--- @param pos integer
+local function __key_cursorset(pos)
+    if __set_cursor(pos) then
+        refresh_input_display()
+    end
+end
+
+local function __key_viewset(pos)
+    if __set_view(pos) then
+        refresh_input_display()
+    end
+end
+
+local function __input_reset()
+    __input = {}
+    __inputview = 1
+    __inputcursor = 1
+end
+
+local function __input_set(str)
+    __input = {}
+
+    for i = 1, #str do
+        table.insert(__input, str:sub(i, i))
+    end
+
+    __set_cursor(w)
+    __set_view(#__input + 1)
+end
+
+--- @type table<string, string>
+local cmd_descriptions =
+{
+    ["whoami"] = "Display the ID of this computer",
+    ["help"] = "Show details about a command",
+    ["ping"] = "Send a ping to all other computers running this program",
+    ["list"] = "List all known commands",
+    ["clear"] = "Clear the message history"
+}
+
+--- @type table<string, function>
+local commands =
+{
+    ["whoami"] = function()
+        recordfmt("I am computer %d", __id)
+    end,
+    ["help"] = function(...)
+        local num = select("#", ...)
+        if num == 0 then
+            recorderr("Expected command as argument")
+        elseif num > 1 then
+            recorderr("Too many arguments")
+        else
+            local cmd = select(1, ...)
+            local desc = cmd_descriptions[cmd]
+            if desc then
+                recordfmt("%s", cmd)
+                recordfmt("  %s", desc)
+            else
+                recorderrfmt("Unknown command: %s", cmd)
+            end
+        end
+    end,
+    ["ping"] = function()
+        record("Sending ping...")
+        send(COMMAND_PING)
+    end,
+    ["list"] = function()
+        record("Available commands:")
+        for cmd, _ in pairs(commands) do
+            recordfmt(" - %s", cmd)
+        end
+    end,
+    ["clear"] = function()
+        __messages = {}
+        __msgcol = {}
+        __msgview = 1
+
+        recordfmt("%s%s", INPUTPREFIX, "clear")
+    end
+}
+
+--- @type string[]
+local __submit_history = {}
+local __history_index = 1
+
+local function __key_submit()
+    local str = native.table.concat(__input)
+
+    if #str > 0 then
+        -- Print the submitted command
+        recordfmt("%s%s", INPUTPREFIX, str)
+
+        -- Check for commands
+        --- @type string, string
+        local cmd, rest = str:match("^(%S+)%s*(.*)$")
+        local command_func = commands[cmd]
+        if command_func then
+            local args = {}
+
+            while #rest > 0 do
+                local quote = rest:find('"')
+                if quote then
+                    -- Get the next quoted argument
+                    local arg = rest:match('^%s*"([^"]*)"')
+                    if arg then
+                        table.insert(args, arg)
+                        rest = rest:sub(quote + #arg + 2)
+                    else
+                        recorderr("Mismatched quote in command arguments")
+                        goto skip_to_display
+                    end
+                else
+                    -- Get the next whitespace-delimited argument
+                    local arg = rest:match("^%s*(%S+)")
+                    if arg then
+                        table.insert(args, arg)
+                        rest = rest:sub(rest:find(arg) + #arg)
+                    else
+                        -- No more arguments
+                        break
+                    end
+                end
+            end
+
+            -- Attempt to deserialize the arguments
+            for i, arg in ipairs(args) do
+                local success, deserialized = pcall(textutils.unserialize, arg)
+                if success then args[i] = deserialized end
+            end
+
+            local success, err = pcall(command_func, table.unpack(args))
+            if not success then
+                recorderrfmt("Error executing command: %s", err)
+            end
+        else
+            recorderrfmt("Unknown command: %s", cmd)
+        end
+
+        ::skip_to_display::
+
+        refresh_message_display()
+
+        -- Restrict the length of the submit history
+        if #__submit_history >= 30 then
+            table.remove(__submit_history, 1)
+        end
+
+        -- Save the command for possible reuse
+        table.insert(__submit_history, str)
+        __history_index = #__submit_history + 1
+
+        -- Clear the input
+        __input_reset()
+        refresh_input_display()
+    end
+end
+
+local function __key_gethistory(direction)
+    local history = __history_index + direction
+    if history == #__submit_history + 1 then
+        __history_index = history
+        __input_reset()
+        refresh_input_display()
+    elseif history >= 1 and history <= #__submit_history then
+        __history_index = history
+        __input_set(__submit_history[history])
+        refresh_input_display()
+    end
+end
+
+local function __key_remove(pos)
+    if pos >= 1 and pos <= #__input then
+        table.remove(__input, pos)
+        if __inputcursor > pos then __set_cursor(__inputcursor - 1) end
+        refresh_input_display()
+    end
+end
+
 exec.loop_forever
 (
     -- wait_interval
@@ -191,7 +573,40 @@ exec.loop_forever
                     end
                 end
             end
-        ),
+        )
+        -- Input-related events
+        :listen(
+            "key",
+            function(key, held)
+                if key == keys.left or key == keys.right then
+                    local direction = key == keys.left and -1 or 1
+                    __key_cursormove(direction, held, false)
+                elseif key == keys.up or key == keys.down then
+                    local direction = key == keys.up and -1 or 1
+                    __key_gethistory(direction)
+                elseif key == keys.enter then
+                    __key_submit()
+                elseif key == keys.home then
+                    __key_cursorset(1)
+                    __key_viewset(1)
+                elseif key == keys.getName("end") then
+                    __key_cursorset(#__input + 1)
+                    __key_viewset(#__input + 1)
+                elseif key == keys.delete then
+                    __key_remove(__inputcursor - #INPUTPREFIX)
+                elseif key == keys.backspace then
+                    __key_remove(__inputcursor - #INPUTPREFIX - 1)
+                end
+            end
+        )
+        :listen(
+            "char",
+            function(char)
+                native.table.insert(__input, __inputcursor, char)
+                __key_cursormove(1, false, false)
+            end
+        )
+    ,
     --quit
     nil
 )
